daa9a7337b65f4ab3b210a3923526f9f5922f3b6
Roflcoptr <sebastian@powersurf.li>
2012-05-07 22:10:55 +0200

    initial import

---
+++ src/TestDriver.java
@@ -0,0 +1,14 @@
+
+public final class TestDriver {
+
+	private TestDriver() {
+		
+	}
+	
+	public static final String URL = "travel.stackexchange.com";
+	
+	public static void main(String[] args) {
+		(new HotQuestionDetector()).detect(URL);
+	}
+	
+}
